444e47cbe183dcac08b8f063b51af14121623320
Yuanhui Yang <yuanhui.yang@outlook.com>
2017-05-18 05:44:16 -0500

    766A. Mahmoud and Longest Uncommon Subsequence

---
+++ 766A.cpp
@@ -0,0 +1,74 @@
+// 766A. Mahmoud and Longest Uncommon Subsequence
+// http://codeforces.com/problemset/problem/766/A
+
+/*
+While Mahmoud and Ehab were practicing for IOI, they found a problem which name was Longest common subsequence. They solved it, and then Ehab challenged Mahmoud with another problem.
+
+Given two strings a and b, find the length of their longest uncommon subsequence, which is the longest string that is a subsequence of one of them and not a subsequence of the other.
+
+A subsequence of some string is a sequence of characters that appears in the same order in the string, The appearances don't have to be consecutive, for example, strings "ac", "bc", "abc" and "a" are subsequences of string "abc" while strings "abbc" and "acb" are not. The empty string is a subsequence of any string. Any string is a subsequence of itself.
+
+Input
+The first line contains string a, and the second line — string b. Both of these strings are non-empty and consist of lowercase letters of English alphabet. The length of each string is not bigger than 105 characters.
+
+Output
+If there's no uncommon subsequence, print "-1". Otherwise print the length of the longest uncommon subsequence of a and b.
+
+Examples
+input
+abcd
+defgh
+output
+5
+input
+a
+a
+output
+-1
+Note
+In the first example: you can choose "defgh" from string b as it is the longest subsequence of string b that doesn't appear as a subsequence of string a.
+*/
+
+#include <iostream> // std::cout; std::cin
+#include <fstream> // std::fstream::open; std::fstream::close;
+#include <ctime>
+#include <cstdlib> // rand
+#include <cassert> // assert
+#include <cctype> // isalnum; isalpha; isdigit; islower; isupper; isspace; tolower; toupper
+#include <cmath> // pow; sqrt; round; fabs; abs; log
+#include <climits> // INT_MIN; INT_MAX; LLONG_MIN; LLONG_MAX; ULLONG_MAX
+#include <cfloat> // DBL_EPSILON; LDBL_EPSILON
+#include <cstring> // std::memset
+#include <algorithm> // std::swap; std::max; std::min; std::min_element; std::max_element; std::minmax_element; std::next_permutation; std::prev_permutation; std::nth_element; std::sort; std::lower_bound; std::upper_bound; std::reverse
+#include <limits> // std::numeric_limits<int>::min; std::numeric_limits<int>::max; std::numeric_limits<double>::epsilon; std::numeric_limits<long double>::epsilon;
+#include <numeric> // std::accumulate; std::iota
+#include <string> // std::to_string; std::string::npos; std::stoul; std::stoull; std::stoi; std::stol; std::stoll; std::stof; std::stod; std::stold; 
+#include <list> // std::list::merge; std::list::splice; std::list::merge; std::list::unique; std::list::sort
+#include <bitset>
+#include <vector>
+#include <deque>
+#include <stack> // std::stack::top; std::stack::pop; std::stack::push
+#include <queue> // std::queue::front; std::queue::back; std::queue::pop; std::queue::push; std::priority_queue; std::priority_queue::top; std::priority_queue::push; std::priority_queue::pop
+#include <set> // std::set::count; std::set::find; std::set::equal_range; std::set::lower_bound; std::set::upper_bound
+#include <map> // std::map::count; std::map::find; std::map::equal_range; std::map::lower_bound; std::map::upper_bound
+#include <unordered_set>
+#include <unordered_map>
+#include <utility> // std::pair; std::make_pair
+#include <iterator>
+#include <functional> // std::less<int>; std::greater<int>
+using namespace std;
+
+class Solution {
+public:
+	int lengthOfLongestUncommonSubsequence(const string& a, const string& b) {
+		return a == b ? -1 : max(a.size(), b.size());
+	}
+};
+
+int main(void) {
+	Solution solution;
+	string a, b;
+	cin >> a >> b;
+	cout << solution.lengthOfLongestUncommonSubsequence(a, b);
+	return 0;
+}
--- README.md
+++ README.md
@@ -1,2 +1,5 @@
 # CodeForces
 Codeforces
+| # | Title | Solution |
+| --- | --- | --- |
+| 766A | Mahmoud and Longest Uncommon Subsequence | [C++](https://github.com/yuanhui-yang/CodeForces/766A.cpp) |
